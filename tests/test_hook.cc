802e0a10b8ea28774bdb7f0a84c65c00a0a4d468
derek <huojichong@163.com>
2023-04-28 00:41:49 -0700

    address

--- tests/test_hook.cc
+++ tests/test_hook.cc
@@ -28,10 +28,48 @@ void test_hook()
     SYLAR_LOG_INFO(g_logger) << " test_sleep";
 }
 
+void text_sock()
+{
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    sockaddr_in addr;
+    memset(&addr,0,sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(80);
+    inet_pton(AF_INET,"104.193.88.123", &addr.sin_addr.s_addr);
+
+    SYLAR_LOG_INFO(g_logger) << "begin connect";
+    int rt = connect(sock,(const sockaddr*)&addr, sizeof(addr));
+    SYLAR_LOG_INFO(g_logger) << "connect rt=" << rt << " errno=" << errno;
+
+    if(rt){
+        return;
+    }
+
+    const char data[] = "GET / HTTP/1.0\r\n\r\n";
+    rt = send(sock, data, sizeof(data), 0);
+    SYLAR_LOG_INFO(g_logger) << "send rt=" << rt << " errno=" << errno;
+    if(rt <=0){
+        return;
+    }
+
+    std::string buff;
+    buff.resize(4096);
+
+    rt = recv(sock,&buff[0], buff.size(), 0);
+    SYLAR_LOG_INFO(g_logger) << "recv rt=" << rt << " errno=" << errno;
+
+    if(rt <= 0){
+        return;
+    }
+
+    buff.resize(rt);
+    SYLAR_LOG_INFO(g_logger) << buff;
+}
 
 int main(int argc ,char** argv)
 {   
-    test_hook();
-
+    // test_hook();
+    text_sock();
     return 0;
 }
