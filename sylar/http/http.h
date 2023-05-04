cd3380542b2cb4273366ea6c6995ed9bcd87fd1c
derek <huojichong@163.com>
2023-05-03 23:32:31 -0700

    http response

--- sylar/http/http.h
+++ sylar/http/http.h
@@ -249,7 +249,8 @@ public:
         return checkGetAs(m_cookies, key, val, def);
     }
 
-    std::ostream& dump(std::ostream& os);
+    std::ostream& dump(std::ostream& os) const;
+    std::string toString() const;
 private:
     HttpMethod m_method;
     HttpStatus m_status;
@@ -264,6 +265,53 @@ private:
     MapType m_headers;
     MapType m_params;
     MapType m_cookies;
+};
+
+
+
+class HttpResponse{
+public: 
+    typedef std::shared_ptr<HttpResponse> ptr;
+    typedef std::map<std::string, std::string, CaseInsensitiveLess> MapType;
+
+    HttpResponse(uint8_t version = 0x11, bool close = true);
+    HttpStatus getStatus() const { return m_status; }
+    uint8_t getVersion() const { return m_version; }
+    const std::string& getBody()const { return m_body; }
+    const std::string& getReason() const { return m_reason; }
+    const MapType& getHeaders() const {return m_headers; }
+
+    void setStatus(HttpStatus v){ m_status = v; }
+    void setVersion(uint8_t v){ m_version = v; }
+    void setBody(const std::string& v) { m_body = v; }
+    void setReason(const std::string& v) { m_reason = v; }
+    void setHeaders(const MapType& v) { m_headers = v; }
+
+    bool isClose()const { return m_close; }
+    void setClose(bool v) { m_close = v; }
+    std::string getHeader(const std::string& key, const std::string& def = "") const;
+    void setHeader(const std::string& key, const std::string& val);
+    void delHeader(const std::string& key);
+
+    template<class T>
+    bool checkGetHeaderAs(const std::string& key,T& val, const T& def = T()) {
+        return checkGetAs(m_headers, key, val, def);
+    }
+
+    template<class T>
+    T getHeaderAs(const std::string& key, const T& def = T()){
+        return getAs(m_headers, key, def);
+    }
+
+    std::ostream& dump(std::ostream& os) const;
+    std::string toString() const;
+private:
+    HttpStatus m_status;
+    uint8_t m_version;
+    bool m_close;
+    std::string m_body;
+    std::string m_reason;
+    MapType m_headers;
 };
 
 
---
+++ sylar/http/http_parser.h
@@ -0,0 +1,17 @@
+#ifndef __SYLAR_HTTP_HTTP_PARSER_H__
+#define __SYLAR_HTTP_HTTP_PARSER_H__
+
+namespace sylar{
+
+namespace http{
+
+
+
+
+
+}
+}
+
+
+
+#endif
