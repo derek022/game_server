3de289b8411c23e5be35c42f3aba389c81b2f3fc
derek <huojichong@163.com>
2023-05-07 19:30:16 -0700

    fix bytearray bug

--- sylar/bytearray.cc
+++ sylar/bytearray.cc
@@ -1,35 +1,31 @@
 #include "bytearray.h"
-#include "log.h"
-#include "endian.h"
-
-#include <string.h>
 #include <fstream>
+#include <sstream>
+#include <string.h>
 #include <iomanip>
 
-namespace sylar
-{
+#include "endian.h"
+#include "log.h"
 
-static Logger::ptr g_logger = SYLAR_LOG_NAME("system");
+namespace sylar {
 
-ByteArray::Node::Node(size_t s) 
+static sylar::Logger::ptr g_logger = SYLAR_LOG_NAME("system");
+
+ByteArray::Node::Node(size_t s)
     :ptr(new char[s])
     ,next(nullptr)
-    ,size(s)
-{
-
+    ,size(s) {
 }
+
 ByteArray::Node::Node()
     :ptr(nullptr)
     ,next(nullptr)
-    ,size(0)
-{
-
+    ,size(0) {
 }
-ByteArray::Node::~Node()
-{
-    if(ptr){
+
+ByteArray::Node::~Node() {
+    if(ptr) {
         delete[] ptr;
-        ptr = nullptr;
     }
 }
 
@@ -40,123 +36,112 @@ ByteArray::ByteArray(size_t base_size)
     ,m_size(0)
     ,m_endian(SYLAR_BIG_ENDIAN)
     ,m_root(new Node(base_size))
-    ,m_cur(m_root) 
-{
-
+    ,m_cur(m_root) {
 }
-ByteArray::~ByteArray()
-{
+
+ByteArray::~ByteArray() {
     Node* tmp = m_root;
-    while(tmp){
+    while(tmp) {
         m_cur = tmp;
         tmp = tmp->next;
         delete m_cur;
     }
-    m_root = nullptr;
-    m_cur = nullptr;
 }
 
-void ByteArray::writeFint8(int8_t value)
-{
-    write(&value, sizeof(value));
+bool ByteArray::isLittleEndian() const {
+    return m_endian == SYLAR_LITTLE_ENDIAN;
 }
-void ByteArray::writeFuint8(uint8_t value)
-{
-    write(&value, sizeof(value));
-}
-void ByteArray::writeFint16(int16_t value)
-{
-    if(m_endian != SYLAR_BYTE_ORDER) {
-        value = byteswap(value);
+
+void ByteArray::setIsLittleEndian(bool val) {
+    if(val) {
+        m_endian = SYLAR_LITTLE_ENDIAN;
+    } else {
+        m_endian = SYLAR_BIG_ENDIAN;
     }
+}
+
+void ByteArray::writeFint8  (int8_t value) {
     write(&value, sizeof(value));
 }
-void ByteArray::writeFuint16(uint16_t value)
-{    
-    if(m_endian != SYLAR_BYTE_ORDER) {
-        value = byteswap(value);
-    }
+
+void ByteArray::writeFuint8 (uint8_t value) {
     write(&value, sizeof(value));
 }
-void ByteArray::writeFint32(int32_t value)
-{
-    if(m_endian != SYLAR_BYTE_ORDER) {
-        value = byteswap(value);
-    }
-    write(&value, sizeof(value));
-}
-void ByteArray::writeFuint32(uint32_t value)
-{
-    if(m_endian != SYLAR_BYTE_ORDER) {
-        value = byteswap(value);
-    }
-    write(&value, sizeof(value));
-}
-void ByteArray::writeFint64(int64_t value)
-{
-    if(m_endian != SYLAR_BYTE_ORDER) {
-        value = byteswap(value);
-    }
-    write(&value, sizeof(value));
-}
-void ByteArray::writeFuint64(uint64_t value)
-{
+void ByteArray::writeFint16 (int16_t value) {
     if(m_endian != SYLAR_BYTE_ORDER) {
         value = byteswap(value);
     }
     write(&value, sizeof(value));
 }
 
-static uint32_t EncodeZigzag32(const int32_t& v)
-{
-    if( v < 0)
-    {
+void ByteArray::writeFuint16(uint16_t value) {
+    if(m_endian != SYLAR_BYTE_ORDER) {
+        value = byteswap(value);
+    }
+    write(&value, sizeof(value));
+}
+
+void ByteArray::writeFint32 (int32_t value) {
+    if(m_endian != SYLAR_BYTE_ORDER) {
+        value = byteswap(value);
+    }
+    write(&value, sizeof(value));
+}
+
+void ByteArray::writeFuint32(uint32_t value) {
+    if(m_endian != SYLAR_BYTE_ORDER) {
+        value = byteswap(value);
+    }
+    write(&value, sizeof(value));
+}
+
+void ByteArray::writeFint64 (int64_t value) {
+    if(m_endian != SYLAR_BYTE_ORDER) {
+        value = byteswap(value);
+    }
+    write(&value, sizeof(value));
+}
+
+void ByteArray::writeFuint64(uint64_t value) {
+    if(m_endian != SYLAR_BYTE_ORDER) {
+        value = byteswap(value);
+    }
+    write(&value, sizeof(value));
+}
+
+static uint32_t EncodeZigzag32(const int32_t& v) {
+    if(v < 0) {
         return ((uint32_t)(-v)) * 2 - 1;
-    }
-    else
-    {
-        return v*2;
+    } else {
+        return v * 2;
     }
 }
 
-static uint64_t EncodeZigzag64(const int64_t& v)
-{
-    if( v < 0)
-    {
+static uint64_t EncodeZigzag64(const int64_t& v) {
+    if(v < 0) {
         return ((uint64_t)(-v)) * 2 - 1;
-    }
-    else
-    {
-        return v*2;
+    } else {
+        return v * 2;
     }
 }
 
-void ByteArray::writeInt32(int32_t value)
-{
+static int32_t DecodeZigzag32(const uint32_t& v) {
+    return (v >> 1) ^ -(v & 1);
+}
+
+static int64_t DecodeZigzag64(const uint64_t& v) {
+    return (v >> 1) ^ -(v & 1);
+}
+
+
+void ByteArray::writeInt32  (int32_t value) {
     writeUint32(EncodeZigzag32(value));
 }
-void ByteArray::writeUint32(uint32_t value)
-{
+
+void ByteArray::writeUint32 (uint32_t value) {
     uint8_t tmp[5];
     uint8_t i = 0;
-    while(value >= 0x80)
-    {
-        tmp[i++] = (value & 0x7F) | 0x80;
-        value >>= 7;
-    }
-    tmp[i++] = value;
-    write(tmp, i);
-}
-void ByteArray::writeInt64(int64_t value)
-{
-    writeUint64(EncodeZigzag64(value));
-}
-void ByteArray::writeUint64(uint64_t value)
-{
-    uint8_t tmp[10];
-    uint8_t i = 0;
-    while(value >= 0x80)
-    {
+    while(value >= 0x80) {
         tmp[i++] = (value & 0x7F) | 0x80;
         value >>= 7;
     }
@@ -164,193 +149,178 @@ void ByteArray::writeUint64(uint64_t value)
     write(tmp, i);
 }
 
-void ByteArray::writeFloat(float value)
-{
+void ByteArray::writeInt64  (int64_t value) {
+    writeUint64(EncodeZigzag64(value));
+}
+
+void ByteArray::writeUint64 (uint64_t value) {
+    uint8_t tmp[10];
+    uint8_t i = 0;
+    while(value >= 0x80) {
+        tmp[i++] = (value & 0x7F) | 0x80;
+        value >>= 7;
+    }
+    tmp[i++] = value;
+    write(tmp, i);
+}
+
+void ByteArray::writeFloat  (float value) {
     uint32_t v;
     memcpy(&v, &value, sizeof(value));
     writeFuint32(v);
 }
-void ByteArray::writeDouble(double value)
-{
+
+void ByteArray::writeDouble (double value) {
     uint64_t v;
     memcpy(&v, &value, sizeof(value));
     writeFuint64(v);
 }
 
-void ByteArray::writeStringF16(const std::string& value)
-{
+void ByteArray::writeStringF16(const std::string& value) {
     writeFuint16(value.size());
     write(value.c_str(), value.size());
 }
-void ByteArray::writeStringF32(const std::string& value)
-{
+
+void ByteArray::writeStringF32(const std::string& value) {
     writeFuint32(value.size());
     write(value.c_str(), value.size());
 }
-void ByteArray::writeStringF64(const std::string& value)
-{
+
+void ByteArray::writeStringF64(const std::string& value) {
     writeFuint64(value.size());
     write(value.c_str(), value.size());
 }
-void ByteArray::writeStringVint(const std::string& value)
-{
+
+void ByteArray::writeStringVint(const std::string& value) {
     writeUint64(value.size());
     write(value.c_str(), value.size());
 }
 
-void ByteArray::writeStringWithoutLength(const std::string& value)
-{
+void ByteArray::writeStringWithoutLength(const std::string& value) {
     write(value.c_str(), value.size());
 }
 
-
-
-int8_t ByteArray::readFint8()
-{
+int8_t   ByteArray::readFint8() {
     int8_t v;
-    read(&v,sizeof(v));
+    read(&v, sizeof(v));
     return v;
 }
-uint8_t ByteArray::readFuint8()
-{
+
+uint8_t  ByteArray::readFuint8() {
     uint8_t v;
-    read(&v,sizeof(v));
+    read(&v, sizeof(v));
     return v;
 }
 
 #define XX(type) \
     type v; \
     read(&v, sizeof(v)); \
-    if(m_endian == SYLAR_BYTE_ORDER) {\
-        return v;   \
-    } else {\
-        return byteswap(v);  \
+    if(m_endian == SYLAR_BYTE_ORDER) { \
+        return v; \
+    } else { \
+        return byteswap(v); \
     }
 
-
-int16_t ByteArray::readFint16()
-{
+int16_t  ByteArray::readFint16() {
     XX(int16_t);
 }
-uint16_t ByteArray::readFuint16()
-{
+uint16_t ByteArray::readFuint16() {
     XX(uint16_t);
 }
-int32_t ByteArray::readFint32()
-{
+
+int32_t  ByteArray::readFint32() {
     XX(int32_t);
 }
-uint32_t ByteArray::readFuint32()
-{
+
+uint32_t ByteArray::readFuint32() {
     XX(uint32_t);
 }
-int64_t ByteArray::readFint64()
-{
+
+int64_t  ByteArray::readFint64() {
     XX(int64_t);
 }
-uint64_t ByteArray::readFuint64()
-{
+
+uint64_t ByteArray::readFuint64() {
     XX(uint64_t);
 }
+
 #undef XX
 
-static int32_t DecodeZigzag32(const uint32_t& v)
-{
-    return (v >> 1) ^ -(v&1);
-}
-
-static int64_t DecodeZigzag64(const uint64_t& v)
-{
-    return (v >> 1) ^ -(v&1);
-}
-
-int32_t ByteArray::readInt32()
-{
+int32_t  ByteArray::readInt32() {
     return DecodeZigzag32(readUint32());
 }
 
-uint32_t ByteArray::readUint32()
-{
+uint32_t ByteArray::readUint32() {
     uint32_t result = 0;
-    for(int i = 0; i< 32; i += 7){
+    for(int i = 0; i < 32; i += 7) {
         uint8_t b = readFuint8();
-        if(b < 0x80)
-        {
+        if(b < 0x80) {
             result |= ((uint32_t)b) << i;
             break;
-        }
-        else
-        {
-            result |= (((uint32_t)(b&0x7f)) << i);
+        } else {
+            result |= (((uint32_t)(b & 0x7f)) << i);
         }
     }
-
     return result;
 }
-int64_t ByteArray::readInt64()
-{
+
+int64_t  ByteArray::readInt64() {
     return DecodeZigzag64(readUint64());
 }
-uint64_t ByteArray::readUint64()
-{
+
+uint64_t ByteArray::readUint64() {
     uint64_t result = 0;
-    for(int i = 0; i< 64; i += 7){
+    for(int i = 0; i < 64; i += 7) {
         uint8_t b = readFuint8();
-        if(b < 0x80)
-        {
+        if(b < 0x80) {
             result |= ((uint64_t)b) << i;
             break;
-        }
-        else
-        {
-            result |= (((uint64_t)(b&0x7f)) << i);
+        } else {
+            result |= (((uint64_t)(b & 0x7f)) << i);
         }
     }
-
     return result;
 }
 
-float ByteArray::readFloat()
-{
+float    ByteArray::readFloat() {
     uint32_t v = readFuint32();
     float value;
     memcpy(&value, &v, sizeof(v));
     return value;
 }
-double ByteArray::readDouble()
-{
+
+double   ByteArray::readDouble() {
     uint64_t v = readFuint64();
     double value;
     memcpy(&value, &v, sizeof(v));
     return value;
 }
 
-std::string ByteArray::readStringF16()
-{
+std::string ByteArray::readStringF16() {
     uint16_t len = readFuint16();
     std::string buff;
     buff.resize(len);
     read(&buff[0], len);
     return buff;
 }
-std::string ByteArray::readStringF32()
-{
+
+std::string ByteArray::readStringF32() {
     uint32_t len = readFuint32();
     std::string buff;
     buff.resize(len);
     read(&buff[0], len);
     return buff;
 }
-std::string ByteArray::readStringF64()
-{
+
+std::string ByteArray::readStringF64() {
     uint64_t len = readFuint64();
     std::string buff;
     buff.resize(len);
     read(&buff[0], len);
     return buff;
 }
-std::string ByteArray::readStringVint()
-{
+
+std::string ByteArray::readStringVint() {
     uint64_t len = readUint64();
     std::string buff;
     buff.resize(len);
@@ -370,7 +340,6 @@ void ByteArray::clear() {
     m_cur = m_root;
     m_root->next = NULL;
 }
-
 
 void ByteArray::write(const void* buf, size_t size) {
     if(size == 0) {
@@ -436,9 +405,8 @@ void ByteArray::read(void* buf, size_t size) {
     }
 }
 
-
 void ByteArray::read(void* buf, size_t size, size_t position) const {
-    if(size > getReadSize()) {
+    if(size > (m_size - position)) {
         throw std::out_of_range("not enough len");
     }
 
@@ -467,14 +435,12 @@ void ByteArray::read(void* buf, size_t size, size_t position) const {
     }
 }
 
-
 void ByteArray::setPosition(size_t v) {
     if(v > m_capacity) {
         throw std::out_of_range("set_position out of range");
     }
     m_position = v;
-    if(m_position > m_size)
-    {
+    if(m_position > m_size) {
         m_size = m_position;
     }
     m_cur = m_root;
@@ -528,7 +494,6 @@ bool ByteArray::readFromFile(const std::string& name) {
     }
     return true;
 }
-
 
 void ByteArray::addCapacity(size_t size) {
     if(size == 0) {
@@ -585,7 +550,6 @@ std::string ByteArray::toHexString() const {
 
     return ss.str();
 }
-
 
 
 uint64_t ByteArray::getReadBuffers(std::vector<iovec>& buffers, uint64_t len) const {
@@ -685,6 +649,5 @@ uint64_t ByteArray::getWriteBuffers(std::vector<iovec>& buffers, uint64_t len) {
     }
     return size;
 }
-    
-} // namespace sylar
 
+}
