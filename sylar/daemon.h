84a2b09d590b345fa611ef740b87c5ff94cb0f70
derek022 <huojichong@163.com>
2023-05-10 04:20:39 +0000

    daemon

---
+++ sylar/daemon.h
@@ -0,0 +1,32 @@
+#ifndef __SYLAR_DAEMON_H__
+#define __SYLAR_DAEMON_H__
+
+#include <unistd.h>
+#include <functional>
+#include "singleton.h"
+
+namespace sylar{
+
+struct ProcessInfo
+{
+    pid_t parent_id = 0;
+    pid_t main_id = 0;
+    uint64_t parent_start_time = 0;
+    uint64_t main_start_time = 0;
+    uint32_t restart_count = 0;
+
+    std::string toString() const;
+};
+
+typedef sylar::Singleton<ProcessInfo> ProcessInfoMgr;
+
+int start_daemon(int argc, char** argv
+                , std::function<int(int argc, char** argv)> main_cb
+                , bool is_daemon);
+
+
+
+}
+
+#endif
+
